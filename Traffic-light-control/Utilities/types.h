92db36464540ebf6f8a044cd42fc07948f7f1bad
Ali Mohamed Taima <es-ali.mohamed2019@alexu.edu.eg>
2022-09-17 10:59:19 +0200

    add types file contains important typedefs that use in the project

---
+++ Traffic-light-control/Utilities/types.h
@@ -0,0 +1,20 @@
+/*
+ * types.h
+ *
+ * Created: 9/17/2022 10:49:44 AM
+ *  Author: Ali Mohamed Taima
+ */ 
+
+
+#ifndef TYPES_H_
+#define TYPES_H_
+/*
+Define important types to use during the project
+*/ 
+typedef unsigned char uint8_t;
+typedef unsigned short uint16_t;
+typedef unsigned int uint32_t;
+
+
+
+#endif /* TYPES_H_ */
