fa566ed3d75561036b9af0d08013476c104341ba
Ali Mohamed Taima <es-ali.mohamed2019@alexu.edu.eg>
2022-09-17 10:59:53 +0200

    add description for each function

--- Traffic-light-control/Application/application.c
+++ Traffic-light-control/Application/application.c
@@ -33,6 +33,15 @@ void App_start(void)
 	{
 		if (mode == NORMAL)
 		{
+			/*
+				This is the normal mode in on demand traffic light control system, and there are the steps to implement this mode
+				1. turn off all LEDs, to prevent the errors if it return from the pedestrain's mode
+				2. turn on cars' Red LED and the pedestrian's Green LED
+				2.1 check if the user push the button during this duration or not, to call the pedestrian's mode
+				3. blink cars' Yellow LED and turn on the pedestrians' Red LED
+				3.1 check if the user push the button during this duration or not, to call the pedestrian's mode
+				4. Trun on cars' Green LED and blink the pedestrians' Yellow LED
+			*/
 			TurnOffLEDs();
 			// turn on cars' Red LED for 5 s and pedestrian's Green LED will be on
 			LED_on(LED_C_RED_PORT, LED_C_RED_PIN);
@@ -71,6 +80,14 @@ void App_start(void)
 }
 ISR(EXT_INT_0)
 {
+	/*
+		Description:
+		This represent the ISR for external interrupt 0
+		1. check the color of cars' LED when the pedestrian push the button
+		2. convert the mode from the normal to pedestrian mode
+		3. turn on the interrupt state the indicate that the user can't implement the ISR again 
+		until finish the first one
+	*/ 
 	if (interrupt_state == INTERRUPT_OFF)
 	{
 		uint8_t value;
@@ -100,6 +117,17 @@ ISR(EXT_INT_0)
 
 void PedestrianMode(void)
 {
+	/*
+	Description:
+	This function implement the pedestrian's mode
+	1. check the color of cars' LED when the pedestrian push the button
+	2. implement the mode according to the cars' LED color
+	3. continue the pedestrain mode until led cars pass 
+		3.1 turn off cars' Red LED
+		3.2 blink cars' Yellow LED and pedestrians' Yellow LED for 5 s
+		3.3 Turn off Pedestrian's Yellow and Green LEDs and cars' Yellow LED
+		3.4 Turn on cars' Red LED and the pedestrian's Green LED, to let cars pass 
+	*/
 	TurnOffLEDs();
 	// Applying pedestrian mode according to cars' LEDs
 	if (color == RED)
@@ -132,6 +160,10 @@ void PedestrianMode(void)
 }
 void TurnOffLEDs(void)
 {
+	/*
+		Description:
+		Turn off all the LEDs (for cars and pedestrains)
+	*/
 	// turn off all cars' LEDs
 	LED_off(LED_C_RED_PORT, LED_C_RED_PIN);
 	LED_off(LED_C_YELLOW_PORT, LED_C_YELLOW_PIN);
@@ -144,6 +176,13 @@ void TurnOffLEDs(void)
 
 void GreenLED(void)
 {
+	/*
+	Description:
+	1. turn on cars' Red LED and the pedestrian's Green LED
+	2. then cars' Yellow LED and pedestrian's Yellow LED will be blink to tell the pedestrians 
+	you should be ready to pass the road
+	3. the cars' Red LED and the pedestrian's Green LED will be on( pedestrians will pass the road)
+	*/
 	LED_on(LED_C_GREEN_PORT, LED_C_GREEN_PIN);
 	LED_on(LED_P_RED_PORT, LED_P_RED_PIN);
 	delay_5s();
@@ -158,12 +197,24 @@ void GreenLED(void)
 }
 void RedLED(void)
 {
+	/*
+		Description: 
+		This function turn on cars' Red LED and Pedestrian's Green LED 
+		To let pedestrians to pass the road
+	*/
 	LED_on(LED_C_RED_PORT, LED_C_RED_PIN);
 	LED_on(LED_P_GREEN_PORT, LED_P_GREEN_PIN);
 	delay_5s();
 }
 void YellowLED(void)
 {
+	/*
+	Description:
+	1. turn on cars' Red LED and blink pedestrian's Yellow LED
+	2. then cars' Yellow LED and  pedestrian's Yellow LED will be blink to tell the pedestrians 
+	you should be ready to pass the road
+	3. the cars' Red LED and the pedestrian's Green LED will be on( pedestrians will pass the road)
+	*/
 	LED_on(LED_P_RED_PORT, LED_P_RED_PIN);
 	toggle_LED_5s(LED_C_YELLOW_PORT, LED_C_YELLOW_PIN);
 	LED_off(LED_C_YELLOW_PORT, LED_C_YELLOW_PIN);
@@ -177,6 +228,10 @@ void YellowLED(void)
 }
 void toggle_2_LEDs_5s(EN_port_t ledPort1, EN_pin_t ledPin1, EN_port_t ledPort2, EN_pin_t ledPin2)
 {
+	/*
+		Description:
+		make a delay for 5 seconds, and blink two LEDs during this duration
+	*/
 	uint32_t overflowCounter = 0;
 
 	TCCR0 |= (1 << 2); // set pin2, 256 prescalar
@@ -198,6 +253,10 @@ void toggle_2_LEDs_5s(EN_port_t ledPort1, EN_pin_t ledPin1, EN_port_t ledPort2, 
 }
 void toggle_LED_5s(EN_port_t ledPort, EN_pin_t ledPin)
 {
+	/*
+		Description:
+		make a delay for 5 seconds, and blink one LED during this duration
+	*/
 	uint32_t overflowCounter = 0;
 
 	TCCR0 |= (1 << 2); // set pin2, 256 prescalar
